906dd2f0c84f61ca097d21dd2a19c945f0df7f8e
Emy Xiao <xiao.emy@gmail.com>
2025-01-19 08:30:22 -0500

    level 1-3 finished + game-over

--- mcwics-frigo/config.py
+++ mcwics-frigo/config.py
@@ -9,39 +9,63 @@ screen = pygame.display.set_mode([WIDTH, HEIGHT])
 fps = 60
 timer = pygame.time.Clock()
 
-next_button = nextButton("images/menu_buttons/next.png", (screen.get_width() - 140) // 2, 700)
+next_button = nextButton("images/menu_buttons/next.png", (screen.get_width() - 65) // 2, 740)
+
 
 ## LEVEL 1
 
 level1_items = {
-    "banana": {"type": "veggies", "file": "images/fridge_items/banana.png", "position": (100, 100)},
-    "orange": {"type": "veggies", "file": "images/fridge_items/orange.png", "position": (100, 200)},
-    "cucumber": {"type": "veggies", "file": "images/fridge_items/cucumber.png", "position": (100, 300)},
-    "lettuce": {"type": "veggies", "file": "images/fridge_items/lettuce.png", "position": (200, 100)},
-    "bread": {"type": "wholeGrains", "file": "images/fridge_items/bread.png", "position": (200, 200)},
-    "oats": {"type": "wholeGrains", "file": "images/fridge_items/oats2.png", "position": (200, 300)},
-    "tofu": {"type": "protein", "file": "images/fridge_items/tofu.png", "position": (300, 100)},
-    "ham": {"type": "protein", "file": "images/fridge_items/ham.png", "position": (300, 200)}
+    "banana": {"type": "veggies", "file": "images/fridge_items/banana.png", "position": (160, 270)},
+    "orange": {"type": "veggies", "file": "images/fridge_items/orange.png", "position": (220, 275)},
+    "cucumber": {"type": "veggies", "file": "images/fridge_items/cucumber.png", "position": (275, 270)},
+    "lettuce": {"type": "veggies", "file": "images/fridge_items/lettuce.png", "position": (160, 370)},
+
+    "bread": {"type": "wholeGrains", "file": "images/fridge_items/bread.png", "position": (220, 375)},
+    "oats": {"type": "wholeGrains", "file": "images/fridge_items/oats2.png", "position": (275, 370)},
+
+    "tofu": {"type": "protein", "file": "images/fridge_items/tofu.png", "position": (180, 460)},
+    "ham": {"type": "protein", "file": "images/fridge_items/ham.png", "position": (250, 460)}
 }
 
 ## LEVEL 2
 
 level2_items = {
-    "banana": {"type": "veggies", "file": "images/fridge_items/banana.png", "position": (100, 100)},
-    "orange": {"type": "veggies", "file": "images/fridge_items/orange.png", "position": (100, 200)},
-    "cucumber": {"type": "veggies", "file": "images/fridge_items/cucumber.png", "position": (100, 300)},
-    "lettuce": {"type": "veggies", "file": "images/fridge_items/lettuce.png", "position": (200, 100)},
-    "strawberry": {"type": "veggies", "file": "images/fridge_items/strawberry.png", "position": (200, 100)},
-    "tomato": {"type": "veggies", "file": "images/fridge_items/tomato.png", "position": (200, 100)},
-    "bread": {"type": "wholeGrains", "file": "images/fridge_items/bread.png", "position": (200, 200)},
-    "oats": {"type": "wholeGrains", "file": "images/fridge_items/oats2.png", "position": (200, 300)},
-    "brown rice": {"type": "wholeGrains", "file": "images/fridge_items/brownrice.png", "position": (200, 100)},
-    "tofu": {"type": "protein", "file": "images/fridge_items/tofu.png", "position": (300, 100)},
-    "ham": {"type": "protein", "file": "images/fridge_items/ham.png", "position": (300, 200)},
-    "chicken": {"type": "protein", "file": "images/fridge_items/chicken.png", "position": (200, 100)},
+    "banana": {"type": "veggies", "file": "images/fridge_items/banana.png", "position": (160, 270)},
+    "orange": {"type": "veggies", "file": "images/fridge_items/orange.png", "position": (220, 275)},
+    "cucumber": {"type": "veggies", "file": "images/fridge_items/cucumber.png", "position": (275, 270)},
+    "lettuce": {"type": "veggies", "file": "images/fridge_items/lettuce.png", "position": (160, 370)},
+    "strawberry": {"type": "veggies", "file": "images/fridge_items/strawberry.png", "position": (220, 375)},
+    "tomato": {"type": "veggies", "file": "images/fridge_items/tomato.png", "position": (275, 370)},
+
+    "bread": {"type": "wholeGrains", "file": "images/fridge_items/bread.png", "position": (180, 460)},
+    "oats": {"type": "wholeGrains", "file": "images/fridge_items/oats2.png", "position": (250, 460)},
+    "brown rice": {"type": "wholeGrains", "file": "images/fridge_items/brownrice.png", "position": (420, 270)},
+
+    "tofu": {"type": "protein", "file": "images/fridge_items/tofu.png", "position": (500, 270)},
+    "ham": {"type": "protein", "file": "images/fridge_items/ham.png", "position": (420, 370)},
+    "chicken": {"type": "protein", "file": "images/fridge_items/chicken.png", "position": (500, 370)},
 }
 
 ## LEVEL 3
 
-level3_items = {}
+level3_items = {
+    "banana": {"type": "veggies", "file": "images/fridge_items/banana.png", "position": (160, 270)},
+    "orange": {"type": "veggies", "file": "images/fridge_items/orange.png", "position": (220, 275)},
+    "cucumber": {"type": "veggies", "file": "images/fridge_items/cucumber.png", "position": (275, 270)},
+    "lettuce": {"type": "veggies", "file": "images/fridge_items/lettuce.png", "position": (160, 370)},
+    "strawberry": {"type": "veggies", "file": "images/fridge_items/strawberry.png", "position": (220, 375)},
+    "tomato": {"type": "veggies", "file": "images/fridge_items/tomato.png", "position": (275, 370)},
+    "carrot": {"type": "veggies", "file": "images/fridge_items/carrot.png", "position": (275, 465)},
+
+    "bread": {"type": "wholeGrains", "file": "images/fridge_items/bread.png", "position": (160, 465)},
+    "oats": {"type": "wholeGrains", "file": "images/fridge_items/oats2.png", "position": (220, 465)},
+    "brown rice": {"type": "wholeGrains", "file": "images/fridge_items/brownrice.png", "position": (400, 270)},
+    "quinoa": {"type": "wholeGrains", "file": "images/fridge_items/quinoa.png", "position": (460, 370)},
+
+    "tofu": {"type": "protein", "file": "images/fridge_items/tofu.png", "position": (520, 270)},
+    "ham": {"type": "protein", "file": "images/fridge_items/ham.png", "position": (400, 370)},
+    "chicken": {"type": "protein", "file": "images/fridge_items/chicken.png", "position": (520, 370)},
+    "salmon": {"type": "protein", "file": "images/fridge_items/salmon.png", "position": (460, 270)},
+    "egg": {"type": "protein", "file": "images/fridge_items/egg.png", "position": (460, 460)}
+}
 
--- mcwics-frigo/main.py
+++ mcwics-frigo/main.py
@@ -9,37 +9,29 @@ buttons_1 = load_fridge(level1_items)
 buttons_2 = load_fridge(level2_items)
 buttons_3 = load_fridge(level3_items)
 
+general_bg = pygame.image.load('images/img/background2.jpg')
+level_bg = pygame.image.load('images/img/open_fridge.png')
+
 bar_1 = Bar(4)
 bar_2 = Bar(6)
 bar_3 = Bar(8)
 
 run = True
-state = "FRIGO_OPEN"
+state = "GAME_OVER"
 level = 1
 coins = 0
 finished = False
+percentages = []
 
 pygame.init()
 
 font = pygame.font.Font('freesansbold.ttf', 18)
-
 pygame.display.set_caption("Food for Thought")
-
 instruction_txt = pygame.image.load('images/img/instructions.png')
 
-buttons_1 = load_fridge(level1_items)
-buttons_2 = load_fridge(level2_items)
-buttons_3 = load_fridge(level3_items)
-
-bar_1 = Bar(4)
-bar_2 = Bar(6)
-bar_3 = Bar(8)
-
-run = True
-state = "FRIGO_OPEN"
-level = 1
-coins = 0
-finished = False
+def points_transition(lst: Bar):
+    global state, level, coins, percentages
+    coins, percentages = points_transitions(screen, lst, font, coins)
 
 while run:
     screen.fill('white')
@@ -76,11 +68,13 @@ while run:
 
     ## LEVEL 1 ##
     if state == "LEVEL_1":
+        screen.blit(general_bg, (0, 0))
+        screen.blit(level_bg, (0, 0))
         for button in buttons_1:
             button.draw()
 
         bar_1.draw(screen)
-        next_button.draw()
+        next_button.draw(screen)
 
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
@@ -99,33 +93,104 @@ while run:
     
     ## LEVEL 2 ##
     elif state == "LEVEL_2":
+        screen.blit(general_bg, (0, 0))
+        screen.blit(level_bg, (0, 0))
         for button in buttons_2:
             button.draw()
         
         bar_2.draw(screen)
-        next_button.draw()
+        next_button.draw(screen)
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                run = False
+            elif event.type == pygame.MOUSEBUTTONDOWN:
+                for button in buttons_2:
+                    if button.check_click():
+                        if button in bar_2:
+                            bar_2.remove_item(button)
+                        else:
+                            bar_2.add_item(button)
+                bar_2.check_click()
+                if next_button.check_click() and len(bar_2.items) == 6:
+                    points_transition(bar_2)
+                    state = "TRANSITION"
 
     ## LEVEL 3 ##
     elif state == "LEVEL_3":
-        pass
+        screen.blit(general_bg, (0, 0))
+        screen.blit(level_bg, (0, 0))
+        for button in buttons_3:
+            button.draw()
+        
+        bar_3.draw(screen)
+        next_button.draw(screen)
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                run = False
+            elif event.type == pygame.MOUSEBUTTONDOWN:
+                for button in buttons_3:
+                    if button.check_click():
+                        if button in bar_3:
+                            bar_3.remove_item(button)
+                        else:
+                            bar_3.add_item(button)
+                bar_3.check_click()
+                if next_button.check_click() and len(bar_3.items) == 8:
+                    points_transition(bar_3)
+                    state = "TRANSITION"
 
     ## TRANSITION STATE ##
     elif state == "TRANSITION": 
-        points_text = font.render(f"Coins: {coins}", True, (0, 0, 0))
-        screen.blit(points_text, ((screen.get_width() - 140) // 2, 20))
-        next_button.draw()
+        screen.blit(general_bg, (0, 0))
+        
+        points_text = pygame.font.Font('freesansbold.ttf', 60).render(f"Coins: {coins}", True, (73, 45, 32))        
+        text_rect = points_text.get_rect(center=(screen.get_width() // 2 + 150, screen.get_height() // 2))
+        screen.blit(points_text, text_rect)
+
+        next_button.draw(screen)
+
+        veg_text = font.render(f"Veggies: {percentages[0] * 100:.2f}%", True, (0, 0, 0))
+        grains_text = font.render(f"Whole Grains: {percentages[1] * 100:.2f}%", True, (0, 0, 0))
+        protein_text = font.render(f"Protein: {percentages[2] * 100:.2f}%", True, (0, 0, 0))
+
+        veg_rect = veg_text.get_rect(center=(screen.get_width() // 2 + 100, screen.get_height() // 2 + 90))
+        grains_rect = grains_text.get_rect(center=(screen.get_width() // 2 + 110, screen.get_height() // 2 + 130))
+        protein_rect = protein_text.get_rect(center=(screen.get_width() // 2 + 100, screen.get_height() // 2 + 170))
+
+        screen.blit(veg_text, veg_rect)
+        screen.blit(grains_text, grains_rect)
+        screen.blit(protein_text, protein_rect)
 
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 run = False
             elif event.type == pygame.MOUSEBUTTONDOWN:
                 if next_button.check_click():
-                    state = f"LEVEL_{level + 1}"
-                    level += 1
+                    if level == 3:
+                        print("Game over")
+                        state = "GAME_OVER"
+                    else:
+                        state = f"LEVEL_{level + 1}"
+                        level += 1
 
     ## GAME OVER ##
     elif state == "GAME_OVER":
-        pass
+        screen.blit(general_bg, (0, 0))
+
+        points_text = pygame.font.Font('freesansbold.ttf', 50).render(f"Game over!", True, (73, 45, 32))        
+        text_rect = points_text.get_rect(center=(screen.get_width() // 2 + 160, screen.get_height() // 2))
+        screen.blit(points_text, text_rect)
+
+        next_button.draw(screen)
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                run = False
+            elif event.type == pygame.MOUSEBUTTONDOWN:
+                if next_button.check_click():
+                    state = "MENU"
     
     pygame.display.flip()
 pygame.quit()
